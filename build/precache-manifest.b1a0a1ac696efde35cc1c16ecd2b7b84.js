133027e72431e5487c2b1a4ce862a358801c1831
Lim Sopheak <limsopheak89@gmail.com>
2022-11-03 11:12:31 +0700

    build

---
+++ build/precache-manifest.b1a0a1ac696efde35cc1c16ecd2b7b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fb0f064be1c0fb364cd9c1ac64fe6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5297f866656f9eaa880e",
+    "url": "/static/css/main.31a76b1f.chunk.css"
+  },
+  {
+    "revision": "161c3eff80eef06e9ac0",
+    "url": "/static/js/2.9e164418.chunk.js"
+  },
+  {
+    "revision": "5297f866656f9eaa880e",
+    "url": "/static/js/main.b4e3bd2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
